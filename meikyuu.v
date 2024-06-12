4d503bd2325eca8a819432b2e82ec83d49c8a680
@ra165429 <ra165429@students.ic.unicamp.br>
2024-06-12 14:40:43 -0300

    Entrega projeto final

---
+++ meikyuu.v
@@ -0,0 +1,320 @@
+module meikyuu(
+    input CLOCK_50,
+    input reset,
+    input btn_up,
+    input btn_down,
+    input btn_left,
+    input btn_right,
+    output [7:0] VGA_R,
+    output [7:0] VGA_G,
+    output [7:0] VGA_B,
+    output VGA_HS,
+    output VGA_VS,
+    output VGA_SYNC_N,
+    output VGA_BLANK_N,
+    output VGA_CLK
+);
+
+reg [9:0] h_counter;
+reg [9:0] v_counter;
+
+wire [9:0] x_pos;
+wire [9:0] y_pos;
+
+reg CLOCK_25;
+
+wire enable_v_counter;
+
+wire active;
+wire active_cube;
+wire active_cube_back;
+
+reg [5:0] mapa_global [7:0][7:0];
+
+
+wire [2:0] mapa_x_pos;
+wire [2:0] mapa_y_pos;
+
+
+assign VGA_R = end_game_1 ? 8'd000 : end_game_2 ? 8'd255 : active_rain ? 8'd70 : active_cube ? 8'd115 : active_cube_back ? 0 : mapa_atual ? 8'd36 : active_house ? 8'd255 : active_house_back ? 0 : active ? 8'h38 : 0;
+assign VGA_G = end_game_1 ? 8'd255 : end_game_2 ? 8'd000 : active_rain ? 8'd78 : active_cube ? 8'd000 : active_cube_back ? 0 : mapa_atual ? 8'd60 : active_house ? 8'd255 : active_house_back ? 0 : active ? 8'h29 : 0;
+assign VGA_B = end_game_1 ? 8'd000 : end_game_2 ? 8'd000 : active_rain ? 8'd80 : active_cube ? 8'd160 : active_cube_back ? 0 : mapa_atual ? 8'd00 : active_house ? 8'd255 : active_house_back ? 0 : active ? 8'h1a : 0;
+
+assign VGA_SYNC_N = 0;
+assign VGA_BLANK_N = 1;
+assign VGA_CLK = CLOCK_25;
+
+wire active_draw;
+wire active_draw_back;
+wire enable_player;
+
+player player(
+  .CLOCK_25(CLOCK_25),
+  .reset(reset),
+  .enable(enable_player),
+  .x_pos_in(x_pos),
+  .y_pos_in(y_pos),
+  .collision(mapa_colision),
+  .btn_up(btn_up),
+  .btn_down(btn_down),
+  .btn_left(btn_left),
+  .btn_right(btn_right),
+  .h_counter(h_counter),
+  .v_counter(v_counter),
+  .x_pos_out(x_pos),
+  .y_pos_out(y_pos),
+  .mapa_pos_x_out(mapa_x_pos),
+  .mapa_pos_y_out(mapa_y_pos),
+  .alt_end_out(alt_end),
+  .active_draw(active_draw),
+  .active_draw_back(active_draw_back),
+);
+
+wire active_rain;
+wire active_rain_draw;
+wire [9:0] x_pos_rain;
+wire [9:0] y_pos_rain;
+
+rain rain(
+  .CLOCK_25(CLOCK_25),
+  .reset(reset),
+  .h_counter(h_counter),
+  .v_counter(v_counter),
+  .x_pos(x_pos_rain),
+  .y_pos(y_pos_rain),
+  .active_rain(active_rain_draw),
+);
+
+wire active_house;
+wire active_house_draw;
+wire active_house_draw_back;
+
+objective house(
+  .CLOCK_25(CLOCK_25),
+  .reset(reset),
+  .h_counter(h_counter),
+  .v_counter(v_counter),
+  .x_map(mapa_x_pos),
+  .y_map(mapa_y_pos),
+  .active_house_draw(active_house_draw),
+  .active_house_draw_back(active_house_draw_back),
+);
+
+always @(posedge CLOCK_50 or posedge reset) begin
+  if(reset) begin
+    CLOCK_25 = 0;
+  end else begin
+	 CLOCK_25 = !CLOCK_25;
+  end
+end
+
+
+always @ (posedge CLOCK_25 or posedge reset) begin
+  if(reset) begin
+    h_counter = 0;
+    v_counter = 0;                                                                                   // aqui é o final
+    mapa_global[0][0] = 10; mapa_global[0][1] = 01; mapa_global[0][2] = 01; mapa_global[0][3] = 05; mapa_global[0][4] = 15; mapa_global[0][5] = 00; mapa_global[0][6] = 01; mapa_global[0][7] = 05;
+    mapa_global[1][0] = 07; mapa_global[1][1] = 07; mapa_global[1][2] = 15; mapa_global[1][3] = 00; mapa_global[1][4] = 03; mapa_global[1][5] = 01; mapa_global[1][6] = 01; mapa_global[1][7] = 05;
+    mapa_global[2][0] = 03; mapa_global[2][1] = 13; mapa_global[2][2] = 11; mapa_global[2][3] = 13; mapa_global[2][4] = 01; mapa_global[2][5] = 04; mapa_global[2][6] = 01; mapa_global[2][7] = 12;
+    mapa_global[3][0] = 07; mapa_global[3][1] = 11; mapa_global[3][2] = 00; mapa_global[3][3] = 10; mapa_global[3][4] = 11; mapa_global[3][5] = 00; mapa_global[3][6] = 10; mapa_global[3][7] = 00;
+    mapa_global[4][0] = 00; mapa_global[4][1] = 06; mapa_global[4][2] = 14; mapa_global[4][3] = 01; mapa_global[4][4] = 01; mapa_global[4][5] = 02; mapa_global[4][6] = 10; mapa_global[4][7] = 12;
+    mapa_global[5][0] = 03; mapa_global[5][1] = 01; mapa_global[5][2] = 06; mapa_global[5][3] = 08; mapa_global[5][4] = 04; mapa_global[5][5] = 01; mapa_global[5][6] = 00; mapa_global[5][7] = 00;
+    mapa_global[6][0] = 01; mapa_global[6][1] = 15; mapa_global[6][2] = 00; mapa_global[6][3] = 10; mapa_global[6][4] = 00; mapa_global[6][5] = 08; mapa_global[6][6] = 00; mapa_global[6][7] = 00;
+    mapa_global[7][0] = 10; mapa_global[7][1] = 01; mapa_global[7][2] = 13; mapa_global[7][3] = 01; mapa_global[7][4] = 02; mapa_global[7][5] = 08; mapa_global[7][6] = 09; mapa_global[7][7] = 09;
+
+  end else begin
+    h_counter = h_counter + 1;
+    if (h_counter == 800) begin
+      h_counter = 0;
+      v_counter = v_counter + 1;
+      if(v_counter == 525) begin
+        v_counter = 0;
+      end
+    end
+  end
+end
+
+reg mapa_atual_reg;
+reg mapa_colision_reg;
+
+always @(posedge CLOCK_25) begin
+  case (mapa_global[mapa_y_pos][mapa_x_pos])
+    // corredor vertical
+    0: begin
+      mapa_atual_reg = mapa_vertical;
+	    mapa_colision_reg = mapa_vertical_colision;
+    end
+    // corredor horizontal
+    1: begin
+      mapa_atual_reg = mapa_horizontal;
+		  mapa_colision_reg = mapa_horizontal_colision;
+    end
+    // faz o L
+    2: begin
+      mapa_atual_reg = mapa_L1;
+		  mapa_colision_reg = mapa_L1_colision;
+    end
+    // Faz o L 90° horário
+    3: begin
+      mapa_atual_reg = mapa_L2;
+		  mapa_colision_reg = mapa_L2_colision;
+    end
+    // Faz o L 180°
+    4: begin
+      mapa_atual_reg = mapa_L3;
+		  mapa_colision_reg = mapa_L3_colision;
+    end
+    // Faz o L 90° anti-horário
+    5: begin
+      mapa_atual_reg = mapa_L4;
+		  mapa_colision_reg = mapa_L4_colision;
+    end
+    // mapa "+"
+    6: begin
+      mapa_atual_reg = mapa_encruzilhada;
+		  mapa_colision_reg = mapa_encruzilhada_colision;
+    end
+    // mapa fechado up
+    7: begin
+      mapa_atual_reg = mapa_fechado_up;
+      mapa_colision_reg = mapa_fechado_up_colision;
+    end
+    8: begin
+      mapa_atual_reg = mapa_fechado_right;
+      mapa_colision_reg = mapa_fechado_right_colision;
+    end
+    9: begin
+      mapa_atual_reg = mapa_fechado_down;
+      mapa_colision_reg = mapa_fechado_down_colision;
+    end
+    10: begin
+      mapa_atual_reg = mapa_fechado_left;
+      mapa_colision_reg = mapa_fechado_left_colision;
+    end
+    // Mapa "T"
+    11: begin
+      mapa_atual_reg = mapa_T_up;
+      mapa_colision_reg = mapa_T_up_colision;
+    end
+    12: begin
+      mapa_atual_reg = mapa_T_right;
+      mapa_colision_reg = mapa_T_right_colision;
+    end
+    13: begin
+      mapa_atual_reg = mapa_T_down;
+      mapa_colision_reg = mapa_T_down_colision;
+    end
+    14: begin
+      mapa_atual_reg = mapa_T_left;
+      mapa_colision_reg = mapa_T_left_colision;
+    end
+    15: begin
+      mapa_atual_reg = mapa_end;
+      mapa_colision_reg = mapa_end_colision || active_house_colision;
+    end
+  endcase
+end
+
+reg end_game_1;
+reg end_game_2;
+reg [2:0] end_game_state;
+
+wire alt_end;
+
+assign enable_player = (~end_game_1) && (~end_game_2);
+
+always @(posedge CLOCK_25 or posedge reset) begin
+  if (reset) begin
+    end_game_1 = 0;
+    end_game_2 = 0;
+    end_game_state = 0;
+  end
+  else begin
+    case (end_game_state)
+      0: begin
+        if(active_house_colision) begin
+          end_game_1 = active ? 1 : 0;
+          end_game_state = 1;
+        end else if (alt_end) begin
+          end_game_2 = active ? 1 : 0;
+          end_game_state = 2;
+        end
+      end
+
+      1: begin
+          end_game_1 = active ? 1 : 0;
+          end_game_state = 1;
+      end
+
+      2:  begin
+        end_game_2 = active ? 1 : 0;
+        end_game_state = 2;
+      end
+    endcase
+  end
+end
+
+assign VGA_HS = (h_counter <= 96) ? 1 : 0;
+assign VGA_VS = (v_counter <= 2) ? 1 : 0;
+assign active = ((v_counter > 2) && (h_counter > 96)) ? 1 : 0;
+assign active_cube = (((v_counter > y_pos) && (h_counter > x_pos) && (v_counter <= y_pos + 20) && (h_counter <= x_pos + 11)) && active_draw) ? 1 : 0;
+assign active_cube_back = (((v_counter > y_pos) && (h_counter > x_pos) && (v_counter <= y_pos + 20) && (h_counter <= x_pos + 11)) && active_draw_back) ? 1 : 0;
+assign active_rain = (((v_counter > y_pos_rain) && (h_counter > x_pos_rain) && (v_counter <= y_pos_rain + 96) && (h_counter <= x_pos_rain + 128)) && active_rain_draw) ? 1 : 0;
+
+assign active_house = (((v_counter > 222 + 35) && (h_counter > 300 + 144) && (v_counter <= 222 + 36 + 35) && (h_counter <= 300 + 40 + 144)) && active_house_draw) ? 1 : 0;
+assign active_house_back = (((v_counter > 222 + 35) && (h_counter > 300 + 144) && (v_counter <= 222 + 36 + 35) && (h_counter <= 300 + 40 + 144)) && active_house_draw_back) ? 1 : 0;
+
+assign active_house_colision = (((y_pos + 20 > 222 + 35) && (x_pos + 11 > 300 + 144) && (y_pos <= 222 + 36 + 35) && (x_pos <= 300 + 40 + 144)) && active_house_draw) ? 1 : 0;
+
+wire mapa_vertical = active && (h_counter < 96 + 100 || h_counter > 96 + 640 - 100) ? 1 : 0; // Parades retas horizontais
+wire mapa_vertical_colision = (x_pos < 96 + 100 || x_pos + 11 > 96 + 640 - 100) ? 1 : 0;
+
+wire mapa_horizontal = active && (v_counter < 2 + 100 || v_counter > 2 + 480 - 100) ? 1 : 0; // Parades retas verticais
+wire mapa_horizontal_colision = (y_pos < 2 + 100 || y_pos + 20 > 2 + 480 - 100) ? 1 : 0;
+
+wire mapa_L1 = active && ((h_counter < 96 + 100 && v_counter < 2 + 100) || (v_counter > 2 + 480 - 100) || (h_counter > 96 + 640 - 100)) ? 1 : 0; //Parede em L
+wire mapa_L1_colision = (x_pos < 96 + 100 && y_pos < 2 + 100) || (y_pos + 20 > 2 + 480 - 100) || (x_pos + 11 > 96 + 640 - 100) ? 1 : 0;
+
+wire mapa_L2 = active && ((h_counter > 96 + 640 - 100 && v_counter < 2 + 100) || (v_counter > 2 + 480 - 100) || (h_counter < 96 + 100)) ? 1 : 0; //Parede em L
+wire mapa_L2_colision = (x_pos + 11 > 96 + 640 - 100 && y_pos < 2 + 100) || (y_pos + 20 > 2 + 480 - 100) || (x_pos < 96 + 100) ? 1 : 0;
+
+wire mapa_L3 = active && ((h_counter > 96 + 640 - 100 && v_counter > 2 + 480 - 100) || (v_counter < 2 + 100) || (h_counter < 96 + 100)) ? 1 : 0; //Parede em L
+wire mapa_L3_colision = (x_pos + 11 > 96 + 640 - 100 && y_pos + 20 > 2 + 480 - 100) || (y_pos < 2 + 100) || (x_pos < 96 + 100) ? 1 : 0;
+
+wire mapa_L4 = active && ((h_counter < 96 + 100 && v_counter > 2 + 480 - 100) || (v_counter < 2 + 100) || (h_counter > 96 + 640 - 100)) ? 1 : 0; //Parede em L
+wire mapa_L4_colision = (x_pos < 96 + 100 && y_pos + 20 > 2 + 480 - 100) || (y_pos < 2 + 100) || (x_pos + 11 > 96 + 640 - 100) ? 1 : 0;
+
+wire mapa_encruzilhada = active && ((h_counter < 96 + 100 || h_counter > 96 + 640 - 100) && (v_counter < 2 + 100 || v_counter > 2 + 480 - 100)) ? 1 : 0; // Encruzilhada
+wire mapa_encruzilhada_colision = (x_pos < 96 + 100 || x_pos + 11 > 96 + 640 - 100) && (y_pos < 2 + 100 || y_pos + 20 > 2 + 480 - 100) ? 1 : 0;
+
+wire mapa_fechado_up = active && (v_counter < 2 + 100 || h_counter < 96 + 100 || h_counter > 96 + 640 - 100) ? 1 : 0; // Parede fechada em cima
+wire mapa_fechado_up_colision = (y_pos < 2 + 100 || x_pos < 96 + 100 || x_pos + 11 > 96 + 640 - 100) ? 1 : 0;
+
+wire mapa_fechado_right = active && (h_counter > 96 + 640 - 100 || v_counter < 2 + 100 || v_counter > 2 + 480 - 100) ? 1 : 0; // Parede fechada na direita
+wire mapa_fechado_right_colision = (x_pos + 11 > 96 + 640 - 100 || y_pos < 2 + 100 || y_pos + 20 > 2 + 480 - 100) ? 1 : 0;
+
+wire mapa_fechado_down = active && (v_counter > 2 + 480 - 100 || h_counter < 96 + 100 || h_counter > 96 + 640 - 100) ? 1 : 0; // Parede fechada em baixo
+wire mapa_fechado_down_colision = (y_pos + 20 > 2 + 480 - 100 || x_pos < 96 + 100 || x_pos + 11 > 96 + 640 - 100) ? 1 : 0;
+
+wire mapa_fechado_left = active && (h_counter < 96 + 100 || v_counter < 2 + 100 || v_counter > 2 + 480 - 100) ? 1 : 0; // Parede fechada na esquerda
+wire mapa_fechado_left_colision = (x_pos < 96 + 100 || y_pos < 2 + 100 || y_pos + 20 > 2 + 480 - 100) ? 1 : 0;
+
+wire mapa_T_up = active && (v_counter < 2 + 100 || (v_counter > 2 + 480 - 100 && h_counter < 96 + 100) || (v_counter > 2 + 480 - 100 && h_counter > 96 + 640 - 100)) ? 1 : 0; // Parede em T
+wire mapa_T_up_colision = (y_pos < 2 + 100 || (y_pos + 20 > 2 + 480 - 100 && x_pos < 96 + 100) || (y_pos + 20 > 2 + 480 - 100 && x_pos + 11 > 96 + 640 - 100)) ? 1 : 0;
+
+wire mapa_T_right = active && (h_counter > 96 + 640 - 100 || (h_counter < 96 + 100 && v_counter < 2 + 100) || (h_counter < 96 + 100 && v_counter > 2 + 480 - 100)) ? 1 : 0; // Parede em T
+wire mapa_T_right_colision = (x_pos + 11 > 96 + 640 - 100 || (x_pos < 96 + 100 && y_pos < 2 + 100) || (x_pos < 96 + 100 && y_pos + 20 > 2 + 480 - 100)) ? 1 : 0;
+
+wire mapa_T_down = active && (v_counter > 2 + 480 - 100 || (v_counter < 2 + 100 && h_counter < 96 + 100) || (v_counter < 2 + 100 && h_counter > 96 + 640 - 100)) ? 1 : 0; // Parede em T
+wire mapa_T_down_colision = (y_pos + 20 > 2 + 480 - 100 || (y_pos < 2 + 100 && x_pos < 96 + 100) || (y_pos < 2 + 100 && x_pos + 11 > 96 + 640 - 100)) ? 1 : 0;
+
+wire mapa_T_left = active && (h_counter < 96 + 100 || (h_counter > 96 + 640 - 100 && v_counter < 2 + 100) || (h_counter > 96 + 640 - 100 && v_counter > 2 + 480 - 100)) ? 1 : 0; // Parede em T
+wire mapa_T_left_colision = (x_pos < 96 + 100 || (x_pos + 11 > 96 + 640 - 100 && y_pos < 2 + 100) || (x_pos + 11 > 96 + 640 - 100 && y_pos + 20 > 2 + 480 - 100)) ? 1 : 0;
+
+wire mapa_end = active && ((h_counter < 96 + 100 && v_counter > 2 + 480 - 100) || (h_counter > 96 + 640 - 100 && v_counter > 2 + 480 - 100)) ? 1 : 0;
+wire mapa_end_colision = ((x_pos < 96 + 100 && y_pos + 20 > 2 + 480 - 100) || (x_pos +  11 > 96 + 640 - 100 && y_pos + 20 > 2 + 480 - 100)) ? 1 : 0;
+
+wire mapa_atual = mapa_atual_reg;
+wire mapa_colision = mapa_colision_reg;
+
+endmodule
---
+++ player.v.bak
@@ -0,0 +1,233 @@
+module player (
+    input CLOCK_25,
+    input reset,
+    input enable,
+    input [9:0] x_pos_in,
+    input [9:0] y_pos_in,
+    input collision,
+    input btn_up,
+    input btn_down,
+    input btn_left,
+    input btn_right,
+    input [9:0] h_counter,
+    input [9:0] v_counter,
+    output [9:0] x_pos_out,
+    output [9:0] y_pos_out,
+    output [2:0] mapa_pos_x_out,
+    output [2:0] mapa_pos_y_out,
+    output alt_end_out,
+    output active_draw,
+    output active_draw_back
+);
+
+//horizontal sync, a = 96, b = 48, c = 640, d = 16, total = 800
+//vertical sync, a = 2, b = 33, c = 480, d = 10, total = 525
+
+
+
+reg [1:0] draw [11:0][20:0];
+
+reg alt_end;
+
+reg [9:0] x_pos = 96 + 48 - 16 + 311;
+reg [9:0] y_pos  = 2 + 33 + 231;
+
+reg [2:0] mapa_x_pos = 0;
+reg [2:0] mapa_y_pos = 7;
+
+
+reg [18:0] move_timer;
+
+reg [2:0] estado = IDLE;
+
+localparam MAX_TIMER = 150000;
+
+localparam NADA       = 3'b000;
+localparam MOVE_UP    = 3'b001;
+localparam MOVE_DOWN  = 3'b010;
+localparam MOVE_RIGHT = 3'b011;
+localparam MOVE_LEFT  = 3'b100;
+localparam IDLE       = 3'b101;
+localparam DEAD       = 3'b110;
+
+
+always @ (posedge CLOCK_25 or posedge reset) begin
+    if (reset) begin
+        x_pos = 96 + 48 - 16 + 311;
+        y_pos = 2 + 33 + 231;
+        estado = IDLE;
+        move_timer = 0;
+        mapa_x_pos = 0;
+        mapa_y_pos = 7;
+        alt_end = 0;
+
+        draw[0][0] = 0;draw[1][0] = 0;draw[2][0] = 0;draw[3][0] = 0;draw[4][0] = 0;draw[5][0] = 1;draw[6][0] = 1;draw[7][0] = 1;draw[8][0] = 1;draw[9][0] = 0;draw[10][0] = 0;
+        draw[0][1] = 0;draw[1][1] = 0;draw[2][1] = 0;draw[3][1] = 1;draw[4][1] = 1;draw[5][1] = 1;draw[6][1] = 1;draw[7][1] = 1;draw[8][1] = 1;draw[9][1] = 1;draw[10][1] = 0;
+        draw[0][2] = 0;draw[1][2] = 0;draw[2][2] = 1;draw[3][2] = 1;draw[4][2] = 1;draw[5][2] = 1;draw[6][2] = 1;draw[7][2] = 1;draw[8][2] = 1;draw[9][2] = 1;draw[10][2] = 0;
+        draw[0][3] = 0;draw[1][3] = 0;draw[2][3] = 1;draw[3][3] = 1;draw[4][3] = 2;draw[5][3] = 1;draw[6][3] = 2;draw[7][3] = 1;draw[8][3] = 1;draw[9][3] = 1;draw[10][3] = 0;
+        draw[0][4] = 0;draw[1][4] = 0;draw[2][4] = 1;draw[3][4] = 1;draw[4][4] = 1;draw[5][4] = 1;draw[6][4] = 1;draw[7][4] = 1;draw[8][4] = 1;draw[9][4] = 1;draw[10][4] = 0;
+        draw[0][5] = 0;draw[1][5] = 0;draw[2][5] = 1;draw[3][5] = 1;draw[4][5] = 2;draw[5][5] = 2;draw[6][5] = 2;draw[7][5] = 1;draw[8][5] = 1;draw[9][5] = 1;draw[10][5] = 0;
+        draw[0][6] = 0;draw[1][6] = 0;draw[2][6] = 1;draw[3][6] = 1;draw[4][6] = 1;draw[5][6] = 1;draw[6][6] = 1;draw[7][6] = 1;draw[8][6] = 1;draw[9][6] = 1;draw[10][6] = 0;
+        draw[0][7] = 0;draw[1][7] = 0;draw[2][7] = 1;draw[3][7] = 1;draw[4][7] = 1;draw[5][7] = 1;draw[6][7] = 1;draw[7][7] = 1;draw[8][7] = 1;draw[9][7] = 1;draw[10][7] = 0;
+        draw[0][8] = 1;draw[1][8] = 1;draw[2][8] = 1;draw[3][8] = 2;draw[4][8] = 2;draw[5][8] = 1;draw[6][8] = 1;draw[7][8] = 1;draw[8][8] = 2;draw[9][8] = 1;draw[10][8] = 1;
+        draw[0][9] = 1;draw[1][9] = 1;draw[2][9] = 1;draw[3][9] = 2;draw[4][9] = 2;draw[5][9] = 2;draw[6][9] = 1;draw[7][9] = 2;draw[8][9] = 2;draw[9][9] = 1;draw[10][9] = 1;
+        draw[0][10] = 1;draw[1][10] = 1;draw[2][10] = 1;draw[3][10] = 2;draw[4][10] = 2;draw[5][10] = 1;draw[6][10] = 1;draw[7][10] = 1;draw[8][10] = 2;draw[9][10] = 1;draw[10][10] = 1;
+        draw[0][11] = 1;draw[1][11] = 1;draw[2][11] = 1;draw[3][11] = 2;draw[4][11] = 2;draw[5][11] = 1;draw[6][11] = 1;draw[7][11] = 1;draw[8][11] = 2;draw[9][11] = 1;draw[10][11] = 1;
+        draw[0][12] = 1;draw[1][12] = 1;draw[2][12] = 1;draw[3][12] = 2;draw[4][12] = 2;draw[5][12] = 1;draw[6][12] = 1;draw[7][12] = 1;draw[8][12] = 2;draw[9][12] = 1;draw[10][12] = 1;
+        draw[0][13] = 0;draw[1][13] = 0;draw[2][13] = 1;draw[3][13] = 2;draw[4][13] = 2;draw[5][13] = 2;draw[6][13] = 1;draw[7][13] = 2;draw[8][13] = 2;draw[9][13] = 1;draw[10][13] = 0;
+        draw[0][14] = 0;draw[1][14] = 0;draw[2][14] = 1;draw[3][14] = 2;draw[4][14] = 2;draw[5][14] = 2;draw[6][14] = 2;draw[7][14] = 2;draw[8][14] = 2;draw[9][14] = 1;draw[10][14] = 0;
+        draw[0][15] = 0;draw[1][15] = 0;draw[2][15] = 1;draw[3][15] = 1;draw[4][15] = 1;draw[5][15] = 1;draw[6][15] = 1;draw[7][15] = 1;draw[8][15] = 1;draw[9][15] = 1;draw[10][15] = 0;
+        draw[0][16] = 0;draw[1][16] = 0;draw[2][16] = 1;draw[3][16] = 1;draw[4][16] = 1;draw[5][16] = 0;draw[6][16] = 0;draw[7][16] = 0;draw[8][16] = 1;draw[9][16] = 1;draw[10][16] = 0;
+        draw[0][17] = 0;draw[1][17] = 0;draw[2][17] = 1;draw[3][17] = 1;draw[4][17] = 1;draw[5][17] = 0;draw[6][17] = 0;draw[7][17] = 0;draw[8][17] = 1;draw[9][17] = 1;draw[10][17] = 0;
+        draw[0][18] = 0;draw[1][18] = 0;draw[2][18] = 1;draw[3][18] = 1;draw[4][18] = 1;draw[5][18] = 0;draw[6][18] = 0;draw[7][18] = 0;draw[8][18] = 1;draw[9][18] = 1;draw[10][18] = 0;
+        draw[0][19] = 0;draw[1][19] = 0;draw[2][19] = 1;draw[3][19] = 1;draw[4][19] = 1;draw[5][19] = 0;draw[6][19] = 0;draw[7][19] = 0;draw[8][19] = 1;draw[9][19] = 1;draw[10][19] = 0;
+
+
+    end else begin
+        x_pos = x_pos_in;
+        y_pos = y_pos_in;
+        case (estado)
+            IDLE: begin
+                if (~btn_left) begin 
+                    estado = MOVE_LEFT;
+                end
+                else if (~btn_down) begin
+                    estado = MOVE_DOWN;
+                end
+                else if (~btn_up) begin
+                    estado = MOVE_UP;
+                end
+                else if (~btn_right) begin
+                    estado = MOVE_RIGHT;
+                end else if (~enable) begin
+                    estado = DEAD;
+                end
+                else begin
+                    estado = IDLE;
+                    move_timer = 0;
+                end
+            end
+            MOVE_LEFT: begin
+                if(x_pos < 96 + 48 - 11) begin
+                    if(mapa_x_pos == 4 && mapa_y_pos == 0) begin
+                       alt_end = 1; 
+                    end
+                    x_pos = 96 + 48 + 640 - 11;
+                    mapa_x_pos = mapa_x_pos - 1;
+                end
+                if(~btn_left) begin
+                    move_timer = move_timer + 1;
+                    estado = MOVE_LEFT;
+                    if(collision) begin
+                        x_pos = x_pos + 1;
+                    end
+
+                    if (move_timer == MAX_TIMER) begin
+                        move_timer = 0;
+                        x_pos = x_pos_in - 1;
+                    end
+                end else begin
+                    estado = IDLE;
+                end
+                if (~enable) begin
+                    estado = DEAD;
+                end
+            end
+
+            MOVE_DOWN: begin
+                if(y_pos > 2 + 33 + 480 - 20) begin
+                    y_pos = 2 + 33;
+                    mapa_y_pos = mapa_y_pos + 1;
+                end
+                if(~btn_down) begin
+                    move_timer = move_timer + 1;
+                    estado = MOVE_DOWN;
+                    if(collision) begin
+                            y_pos = y_pos - 1;
+                        end
+                    if (move_timer == MAX_TIMER) begin
+                        move_timer = 0;
+                        y_pos = y_pos_in + 1;
+                    end
+                end else begin
+                    estado = IDLE;
+                end
+                if (~enable) begin
+                    estado = DEAD;
+                end
+            end
+
+            MOVE_UP: begin
+                if(y_pos < 2 + 33) begin
+                    if(mapa_x_pos == 4 && mapa_y_pos == 0) begin
+                       alt_end = 1; 
+                    end
+                    y_pos = 2 + 33 + 480 - 20;
+                    mapa_y_pos = mapa_y_pos - 1;
+                end
+                if(~btn_up) begin
+                    move_timer = move_timer + 1;
+                    estado = MOVE_UP;
+                    if(collision) begin
+                            y_pos = y_pos + 1;
+                        end
+                    if (move_timer == MAX_TIMER) begin
+                        move_timer = 0;
+                        y_pos = y_pos_in - 1;
+                    end
+                end else begin
+                    estado = IDLE;
+                end
+                if (~enable) begin
+                    estado = DEAD;
+                end
+            end
+
+            MOVE_RIGHT: begin
+                if(x_pos > 96 + 48 + 640 - 11) begin
+                    if(mapa_x_pos == 4 && mapa_y_pos == 0) begin
+                       alt_end = 1; 
+                    end
+                    x_pos = 96 + 48 - 11;
+                    mapa_x_pos = mapa_x_pos + 1;
+                end
+                if(~btn_right) begin
+                    move_timer = move_timer + 1;
+                    estado = MOVE_RIGHT;
+                    if(collision) begin
+                            x_pos = x_pos - 1;
+                        end
+
+                    if (move_timer == MAX_TIMER && ~collision) begin
+                        move_timer = 0;
+                        x_pos = x_pos_in + 1;
+                    end
+                end else begin
+                    estado = IDLE;
+                end
+                if (~enable) begin
+                    estado = DEAD;
+                end
+            end
+            DEAD: begin
+                estado = DEAD;
+            end
+            default: begin
+                estado = IDLE;
+            end
+        endcase           
+    end
+end
+
+assign x_pos_out = x_pos;
+assign y_pos_out = y_pos;
+
+assign mapa_pos_x_out = mapa_x_pos;
+assign mapa_pos_y_out = mapa_y_pos;
+
+assign active_draw = (draw[h_counter - x_pos][v_counter - y_pos] == 1) ? 1 : 0;
+assign active_draw_back = (draw[h_counter - x_pos][v_counter - y_pos] == 2) ? 1 : 0;
+
+assign alt_end_out = alt_end;
+
+endmodule
